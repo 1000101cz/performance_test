ca7e0fd861ef17ab5617642629974852f6ee0f33
1000101cz <1000101cz@seznam.cz>
2021-06-05 01:24:50 +0200

    cs edit

--- pc_test.c
+++ pc_test.c
@@ -2,22 +2,22 @@
  ********************************************
  ***                                      ***
  ***    NAME:    PC performance test      ***
- ***    VERSION: 0.0.2                    ***
+ ***    VERSION: 0.0.3                    ***
  ***    AUTHOR:  MAROUSEK S.              ***
  ***    DATE:    2021/5/30                ***
  ***                                      ***
  ********************************************
  ********************************************/
 
-/**************************************************************************
- *                                                                        *
- * Info:                                                                  *
- *                                                                        *
- *    compile:          'make'   (or 'gcc pc_test.c -lpthread -o test')   *
- *    execute:          './test'                                          *
- *    remove binary:    'make clean'                                      *
- *                                                                        *
- **************************************************************************/
+/******************************************************************************
+ *                                                                            *
+ * Info:                                                                      *
+ *                                                                            *
+ *    compile:          'make'   (or 'gcc pc_test.c -lpthread -o0 -o test')   *
+ *    execute:          './test'                                              *
+ *    remove binary:    'make clean'                                          *
+ *                                                                            *
+ ******************************************************************************/
 
 #include <stdio.h>
 #include <pthread.h>
@@ -32,33 +32,27 @@ void *thread2(void *);
 void *thread3(void *);
 void *thread4(void *);
 
-_Bool thread1c = 0;
-_Bool thread2c = 0;
-_Bool thread3c = 0;
-_Bool thread4c = 0;
-
+_Bool thread1c;
 long long number_of_operations1 = 0;
+_Bool thread2c;
 long long number_of_operations2 = 0;
+_Bool thread3c;
 long long number_of_operations3 = 0;
+_Bool thread4c;
 long long number_of_operations4 = 0;
 
 long long number_of_operations = 0;
 _Bool end = 0;
 
-int main()
+int main(int argc, char *argv[])
 {
 	FILE *save_data;
-	void *(*thr_function[])(void *) = { threadTimeCount,thread1,thread2,thread3,thread4 };
+	void *(*thr_function[])(void *) =
+	    { threadTimeCount, thread1, thread2, thread3, thread4 };
 	pthread_t threads[5];
-	pthread_create(&threads[0], NULL, thr_function[0], NULL);
-	pthread_create(&threads[1], NULL, thr_function[1], NULL);
-	pthread_create(&threads[2], NULL, thr_function[2], NULL);
-	pthread_create(&threads[3], NULL, thr_function[3], NULL);
-	pthread_create(&threads[4], NULL, thr_function[4], NULL);
-	thread1c = 1;
-	thread2c = 1;
-	thread3c = 1;
-	thread4c = 1;
+	for (int i = 0; i < 5; i++) {
+		pthread_create(&threads[i], NULL, thr_function[i], NULL);
+	}
 
 	char *bench_txt = "pc_test_bench.txt";
 
@@ -77,34 +71,34 @@ int main()
 		number_of_operations++;
 	}
 	printf("\r%lld", divide);
-	while (thread1c || thread2c || thread3c || thread4c) {}
+	while (thread1c || thread2c || thread3c || thread4c) {
+	}
 
 	printf("\r");
-	/*printf("Number0:  %lld\n",number_of_operations);
-	printf("Number1:  %lld\n",number_of_operations1);
-	printf("Number2:  %lld\n",number_of_operations2);
-	printf("Number3:  %lld\n",number_of_operations3);
-	printf("Number4:  %lld\n",number_of_operations4);*/
+	if (argc > 1) {
+		printf
+		    ("Number0:  %lld\nNumber1:  %lld\nNumber2:  %lld\nNumber3:  %lld\nNumber4:  %lld\n",
+		     number_of_operations, number_of_operations1,
+		     number_of_operations2, number_of_operations3,
+		     number_of_operations4);
+	}
 
-	number_of_operations = number_of_operations + number_of_operations1 + number_of_operations2 + number_of_operations3 + number_of_operations4;
+	number_of_operations =
+	    number_of_operations + number_of_operations1 +
+	    number_of_operations2 + number_of_operations3 +
+	    number_of_operations4;
 
 	printf("Computation score     %f\n",
 	       (float)(number_of_operations / RUN_TIME / 18.81));
-	pthread_detach(threads[0]);
-	pthread_create(&threads[0], NULL, thr_function[0], NULL);
-
-	pthread_detach(threads[1]);
-	pthread_join(threads[1], NULL);
-	pthread_detach(threads[2]);
-	pthread_join(threads[2], NULL);
-	pthread_detach(threads[3]);
-	pthread_join(threads[3], NULL);
-	pthread_detach(threads[4]);
-	pthread_join(threads[4], NULL);
+	for (int i = 0; i < 5; i++) {
+		pthread_detach(threads[i]);
+		pthread_join(threads[i], NULL);
+	}
 
 	save_data = fopen(bench_txt, "w");
 
 	/* Test memory access speed */
+	pthread_create(&threads[0], NULL, thr_function[0], NULL);
 	number_of_operations = 0;
 	end = 0;
 	long long file_out;
@@ -147,15 +141,15 @@ void *threadTimeCount(void *v)
 	clock_t start = clock();
 	while ((clock() - start) * 1000 / CLOCKS_PER_SEC < RUN_TIME) ;
 	end = 1;
+	thread1c = thread2c = thread3c = thread4c = 0;
 	return 0;
 }
 
-void *thread1(void *v)
+long long computationThread(long long number_o_o, _Bool *threadC)
 {
-	long long number_of_operations11 = 0;
 	long long divide;
-	while (!end) {
-		divide = 1 + number_of_operations11;
+	while (*threadC) {
+		divide = 1 + number_o_o;
 		for (int i = 1; i <= 18; i++) {
 			divide = divide * i;
 			divide++;
@@ -164,76 +158,43 @@ void *thread1(void *v)
 			divide--;
 			divide = divide / i;
 		}
-		number_of_operations11++;
+		number_o_o++;
 	}
-	thread1c = 0;
+	return number_o_o;
+}
+
+void *thread1(void *v)
+{
+	thread1c = 1;
+	long long number_of_operations11 = computationThread(0, &thread1c);
 	number_of_operations1 = number_of_operations11;
-	printf("\r%lld", divide);
+	printf("\r%lld", number_of_operations11);
 	return 0;
 }
 
 void *thread2(void *v)
 {
-	long long number_of_operations22 = 0;
-	long long divide;
-	while (!end) {
-		divide = 1 + number_of_operations22;
-		for (int i = 1; i <= 18; i++) {
-			divide = divide * i;
-			divide++;
-		}
-		for (int i = 1; i <= 18; i++) {
-			divide--;
-			divide = divide / i;
-		}
-		number_of_operations22++;
-	}
-	thread2c = 0;
+	thread2c = 1;
+	long long number_of_operations22 = computationThread(0, &thread2c);
 	number_of_operations2 = number_of_operations22;
-	printf("\r%lld", divide);
+	printf("\r%lld", number_of_operations22);
 	return 0;
 }
 
 void *thread3(void *v)
 {
-	long long number_of_operations33 = 0;
-	long long divide;
-	while (!end) {
-		divide = 1 + number_of_operations33;
-		for (int i = 1; i <= 18; i++) {
-			divide = divide * i;
-			divide++;
-		}
-		for (int i = 1; i <= 18; i++) {
-			divide--;
-			divide = divide / i;
-		}
-		number_of_operations33++;
-	}
-	thread3c = 0;
+	thread3c = 1;
+	long long number_of_operations33 = computationThread(0, &thread3c);
 	number_of_operations3 = number_of_operations33;
-	printf("\r%lld", divide);
+	printf("\r%lld", number_of_operations33);
 	return 0;
 }
 
 void *thread4(void *v)
 {
-	long long number_of_operations44 = 0;
-	long long divide;
-	while (!end) {
-		divide = 1 + number_of_operations44;
-		for (int i = 1; i <= 18; i++) {
-			divide = divide * i;
-			divide++;
-		}
-		for (int i = 1; i <= 18; i++) {
-			divide--;
-			divide = divide / i;
-		}
-		number_of_operations44++;
-	}
-	thread4c = 0;
+	thread4c = 1;
+	long long number_of_operations44 = computationThread(0, &thread4c);
 	number_of_operations4 = number_of_operations44;
-	printf("\r%lld", divide);
+	printf("\r%lld", number_of_operations44);
 	return 0;
 }
